cd0f708dcbc9146f72b5e1f48a49f6a45043df89
Yash Singh <yashsingh@Yashs-MacBook-Air.local>
2024-07-01 17:24:27 +0530

    Complete HomePage UI

---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useState } from "react";
+import HomeCard from "./HomeCard";
+import { useRouter } from "next/navigation";
+import MeetingModel from "./MeetingModal";
+
+
+const MeetingTypeList = () => {
+
+  const router = useRouter();
+  const createMeeting = () => {
+    
+  }
+
+  const [meetingState, setMeetingState] =
+       useState<'isScheduleMeeting' | 'isJoiningMeeting' | 
+       'isInstantMeeting' | undefined >()
+
+  return (
+    <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+      <HomeCard 
+        img="/icons/add-meeting.svg"
+        title="New Meeting"
+        description="Start an instant meeting"
+        handleClick={() => setMeetingState('isInstantMeeting')}
+      className="bg-orange-1"
+      />
+      <HomeCard 
+      img="/icons/schedule.svg"
+      title="Schedule Meeting"
+      description="Plan your meeting"
+      handleClick={() => setMeetingState('isScheduleMeeting')}
+      className="bg-blue-1"
+      />
+      <HomeCard 
+      img="/icons/recordings.svg"
+      title="View Recordings"
+      description="Check out your recordings"
+      handleClick={() => router.push("/recordings")}
+      className="bg-purple-1"
+
+      />
+      <HomeCard 
+      img="/icons/join-meeting.svg"
+      title="Join Meeting"
+      description="via invitation link"
+      handleClick={() => setMeetingState('isJoiningMeeting')}
+      className="bg-yellow-1"
+
+      />
+      
+
+      <MeetingModel 
+        isOpen={meetingState === 'isInstantMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title="Start a instant meeting"
+        className="text-center"
+        buttonText="Start meeting"
+        handleClick={createMeeting}
+      />
+    </section>
+  );
+};
+
+export default MeetingTypeList;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import MobileNav from '../MobileNav'
+import MobileNav from './MobileNav'
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
